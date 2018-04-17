05f6269224ac8fc3222dbef803ca6329da2b9fa9
Jared <jared.michael.wood@gmail.com>
2018-04-16 19:19:39 -0500

    Started working on Add Login and Signup pages

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     body = db.Column(db.String(500))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body):
         self.title = title
@@ -32,7 +33,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref='owner_id')
+    blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password, blogs):
         self.uesrname = username
